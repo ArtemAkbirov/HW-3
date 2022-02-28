b7c29ad131df4f8f89c9d0311e17b2ccd7a2ad56
Artem <artem-acc@mail.ru>
2022-02-28 19:43:55 +0300

    Task11(NePravilno)

---
+++ Task 11/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N <= 0)
+{
+    Console.WriteLine("Вы ввели число равное нулю или меньше");
+    return;
+}
+
+if (N % 2 == 0)
+{
+    for (int i = 0; i <= N; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+else
+{
+    for (int i = 0; i < N; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
