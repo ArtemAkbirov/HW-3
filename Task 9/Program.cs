bc4ff2ba49ce7f7f1719fab308f8a60583adc360
Artem <artem-acc@mail.ru>
2022-02-28 19:05:19 +0300

    Task9

---
+++ Task 9/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (a!=0)
+{
+    if ((a%10) % 2 != 0)
+    {
+        result++;
+    }
+    a/=10;
+}
+Console.WriteLine(result);
