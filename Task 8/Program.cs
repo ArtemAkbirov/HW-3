02c612b5d2236ad496d581bae93b0119b380a4c5
Artem <artem-acc@mail.ru>
2022-02-28 18:50:26 +0300

    Task8

---
+++ Task 8/Program.cs
@@ -0,0 +1,25 @@
+﻿int cub = Convert.ToInt32(Console.ReadLine());
+int left = 0;
+int right = cub;
+int centr = 0;
+
+if (cub < 0)
+{
+    Console.WriteLine("Было введено отрицатеельное число");
+}
+
+do
+{
+    centr = (left + right) / 2;
+    if (Math.Pow(centr, 3) < cub)
+    {
+        left = centr;
+    }
+    else
+    {
+        right = centr;
+    }
+}
+while (Math.Pow(centr, 3) != cub);
+
+Console.WriteLine(centr);
